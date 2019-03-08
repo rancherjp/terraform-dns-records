085198eeb321fbde9df3eb521fcfc1c4ea668fff
Naoki Aoyama <9667078+aoya6i@users.noreply.github.com>
2019-03-09 08:39:20 +0900

    Update recordsets.tf
    add: growi.rancher.jp
    add: codimd.rancher.jp

--- terraform/envs/rancher-jp/recordsets.tf
+++ terraform/envs/rancher-jp/recordsets.tf
@@ -26,5 +26,22 @@ variable "recordsets" {
       ttl     = "300"
       rrdatas = "rancherjp.github.io."
     },
+    {
+      // CNAME: growi.rancher.jp
+      name    = "growi."
+      type    = "CNAME"
+      ttl     = "300"
+      rrdatas = "growi01.tyo1.conoha.aoya6i.net."
+    },
+    {
+      // CNAME: growi.rancher.jp
+      name    = "codimd."
+      type    = "CNAME"
+      ttl     = "300"
+      rrdatas = "growi01.tyo1.conoha.aoya6i.net."
+    },
+    
+    
+    
   ]
 }
